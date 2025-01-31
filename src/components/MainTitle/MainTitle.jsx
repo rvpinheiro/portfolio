3d2ad10e1f133ed60551dd9500b3d3dc5063a042
Rui Pinheiro <rui.pinheiro@askblue.pt>
2025-01-31 18:21:09 +0000

    Homepage layout changes

--- src/components/MainTitle/MainTitle.jsx
+++ src/components/MainTitle/MainTitle.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from './MainTitle.module.css'
 
-const MainTitle = ({ title, subtitle }) => {
+const MainTitle = ({ title, author }) => {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>{title}</h1>
-            <p className={styles.subtitle}>{subtitle}</p>
+            <p className={styles.author}>{author}</p>
         </div>
     )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,29 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import MainTitle from '../../components/MainTitle/MainTitle';
-import Menu from '../../components/Menu/Menu';
-import Button from '../../components/Button/Button'
 import styles from './Home.module.css';
 
 const Home = () => {
-    const [title, setTitle] = useState('Title');
-    const [subtitle, setSubtitle] = useState("Subtitle");
-    const [buttonLink, setButtonLink] = useState('/about');
-
-    const updateContent = (newTitle, newSubtitle, newButtonLink) => {
-        setTitle(newTitle);
-        setSubtitle(newSubtitle);
-        setButtonLink(newButtonLink);
-    };
+    const title = "Once a new technology rolls over you, if you're not part of the steamroller, you're part of the road.";
+    const author = "Stewart Brand";
 
     return (
         <div className={styles.homeContainer}>
             <div className={styles.mainContent}>
-                <MainTitle title={title} subtitle={subtitle} />
-                <Button label="More" to={buttonLink} />
-                <Menu
-                    items={['section1', 'section2', 'section3']}
-                    updateContent={updateContent}
-                />
+                <MainTitle title={title} author={author} />
             </div>
         </div>
     );
