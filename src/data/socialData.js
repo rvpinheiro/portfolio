6f48c5fc279a8cfceef6f8396ff835cba6c8add6
Rui Pinheiro <rui.pinheiro@askblue.pt>
2025-02-28 23:19:42 +0000

    CV Uploaded to assets

--- src/data/socialData.js
+++ src/data/socialData.js
@@ -3,7 +3,7 @@ const socialData = {
         { platform: "linkedin", link: "https://www.linkedin.com/in/ruivpinheiro", hoverColor: "#0077b5" },
         { platform: "github", link: "https://github.com/rvpinheiro", hoverColor: "#AC69DC" },
         { platform: "whatsapp", link: "https://wa.me/+351911530924", hoverColor: "#25D366" },
-        { platform: "download", link: "/cv.pdf", hoverColor: "#4CAF50" },
+        { platform: "download", link: "/assets/docs/CV.pdf", hoverColor: "#4CAF50" },
     ],
 };
 
